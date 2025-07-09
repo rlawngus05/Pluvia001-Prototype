1536b139b7acff9c9d5f92e7ca32d814dfcfa07c
abyss_tutor <yejun6210@gmail.com>
2025-07-09 21:28:31 +0900

    feat : HealthManager을 싱글톤화 함

--- Assets/Scripts/Player/HealthManager.cs
+++ Assets/Scripts/Player/HealthManager.cs
@@ -2,17 +2,32 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
+    public static HealthManager Instance { get; private set; }
+
     [SerializeField] private int _health;
 
     private const int MAX_HEALTH = 100;
     private const int MIN_HEALTH = 0;
 
-    public int GetHealth() { return _health; }
+    private void Awake()
+    {
+        // Singleton 설정
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬 전환 시 파괴 방지
+        }
+        else
+        {
+            Destroy(gameObject); // 중복 인스턴스 제거
+        }
+    }
+
+    public int GetHealth() => _health;
 
     public void IncreaseHealth(int value = 1)
     {
         _health += value;
-
         if (_health > MAX_HEALTH)
         {
             _health = MAX_HEALTH;
@@ -22,13 +37,12 @@ public class HealthManager : MonoBehaviour
     public void DecreaseHealth(int value = 1)
     {
         _health -= value;
-
         if (_health < MIN_HEALTH)
         {
             _health = MIN_HEALTH;
         }
     }
-    
+
     // * 이 코드는 오직 테스트 용도로만 사용됨
     // private void Update()
     // {
