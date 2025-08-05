ca1d494d2962875232ef30f8d17e362e620d51b7
abyss_tutor <yejun6210@gmail.com>
2025-08-05 22:03:46 +0900

    Refactor : PlayerController.cs 코드 리펙토링

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,13 +12,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _earlyJumpDivder;
     [SerializeField] private float _unholdJumpGravityScale;
     [SerializeField] private float _maxFallVelocity;
-    [SerializeField] private Animator _animator;
+    [SerializeField] private PlayerState _currentState;
+
     private float _originGravityScale;
     private bool _isJump;
 
+    private Animator _animator;
     private Rigidbody2D _rb;
-    private SpriteRenderer _spr;
-    [SerializeField] private PlayerState _currentState;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
@@ -32,7 +33,9 @@ public class PlayerController : MonoBehaviour
         DontDestroyOnLoad(gameObject);
 
         _rb = GetComponent<Rigidbody2D>();
-        _spr = GetComponent<SpriteRenderer>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+
         _currentState = PlayerState.Idle;
         _isJump = false;
         _originGravityScale = 1.0f;
@@ -45,30 +48,21 @@ public class PlayerController : MonoBehaviour
         {
             float moveDirection = Input.GetAxisRaw("Horizontal");
             SetVelocityX(moveDirection, _moveSpeed);
+            
             //* 걷는 애니메이션 + 좌우 전환 로직
             if (moveDirection == 0.0f)
             {
                 _animator.SetBool("isWalking", false);
             }
             else
-            {   
+            {
                 _animator.SetBool("isWalking", true);
-                if (moveDirection == 1.0f) { _spr.flipX = true; }
-                if (moveDirection == -1.0f) { _spr.flipX = false; }
+                if (moveDirection == 1.0f) { _spriteRenderer.flipX = true; }
+                if (moveDirection == -1.0f) { _spriteRenderer.flipX = false; }
             }
-            
         }
 
         _rb.linearVelocityY = Mathf.Max(_rb.linearVelocityY, -_maxFallVelocity); //* 낙하 속도 최대치 설정
-        //* 점프 애니메이션 실행
-        if (_isJump) { _animator.SetBool("isJumping", _isJump); }
-        if (_rb.linearVelocityY < 0 && _isJump) { _animator.SetBool("isFalling", true); }
-        if (!_isJump)
-        {
-            _animator.SetBool("isJumping", _isJump);
-            _animator.SetBool("isFalling", false);
-        }
-        
     }
 
     private void Update()
@@ -95,6 +89,15 @@ public class PlayerController : MonoBehaviour
                     _originGravityScale = _rb.gravityScale;
                     _rb.gravityScale = _unholdJumpGravityScale;
                 }
+            }
+
+            //* 점프 애니메이션 실행
+            if (_isJump) { _animator.SetBool("isJumping", _isJump); }
+            if (_rb.linearVelocityY < 0 && _isJump) { _animator.SetBool("isFalling", true); }
+            if (!_isJump)
+            {
+                _animator.SetBool("isJumping", _isJump);
+                _animator.SetBool("isFalling", false);
             }
 
             //* 인벤토리 열기 로직
